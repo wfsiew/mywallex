4058c3e64855ac4a01de4faec73c8ab98e36b4c8
Siew Wing Fei <wingfei.siew@nova-hub.com>
2023-08-18 13:09:27 +0800

    i

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { WallexWebhookDto } from './dto/wallex.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('backend/backend_response')
+  async Webhook(@Body() data: WallexWebhookDto) {
+    console.log('called backend response');
+    return {
+      success: 'ok',
+      data
+    }
   }
 }
